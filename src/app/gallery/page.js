4e70a6779b7c515f705ab2b0a48335676ec9ff97
Ebun22 <subeydavid22@gmail.com>
2023-09-19 20:36:41 +0100

    fetched images

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { UseAuthContext } from '@/Context/AuthContext';
 import { useStateContext } from '@/Context/context';
 import { Navbar } from '../components';
-import  images from '../../images.json';
+import images from '../../../images';
 
 function Home() {
     const { error, setError, loading, setLoading } = useStateContext()
@@ -14,7 +14,30 @@ function Home() {
         <div className="p-4 pl-8">
             <Navbar />
             <div>
-{console.log(images)}
+                <div>
+                <h1 className="text-4xl font-bold">Photos</h1>
+                <span className="text-sky-400">Recent</span>
+                </div>
+                
+                <div>
+                    <input type='text' placeholder='Search' className='bg-tarnspartent p-2 border-2 border-slate-300 rounded-lg' />
+                    <span>
+                        
+                    </span>
+                </div>
+            </div>
+            <div className='grid grid-cols-4 gap-4 w-full h-full my-14 '>
+                {images.map((item, index) => (
+                    <div key={index} className='h-64 w-full'>
+                        <Image 
+                        width={250}
+                        height={250}
+                        alt={item.name}
+                        src={item.path}
+                        className='w-full h-full '
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     )
